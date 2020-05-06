14a51989274c25b0150da5b5d4906b3dc9472b2c
aravena99 <scottaravena@gmail.com>
2020-05-06 14:23:04 -0400

    Road and Player update, ShaderToy for road

--- src/app.js
+++ src/app.js
@@ -9,10 +9,14 @@
 import { WebGLRenderer, PerspectiveCamera, Vector3 } from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { SeedScene } from 'scenes';
+import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer.js';
+import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass.js';
+import { UnrealBloomPass } from 'three/examples/jsm/postprocessing/UnrealBloomPass.js';
+
+
 
 // Initialize core ThreeJS components
-const camera = new PerspectiveCamera();
-const scene = new SeedScene(camera);
+
 const renderer = new WebGLRenderer({ antialias: true });
 
 // Set up renderer, canvas, and minor CSS adjustments
@@ -23,6 +27,15 @@ document.body.style.margin = 0; // Removes margin around page
 document.body.style.overflow = 'hidden'; // Fix scrolling
 document.body.appendChild(canvas);
 
+// Global variables for ShaderToy
+global.uniforms = {
+    iTime: { value: 0 },
+    iResolution:  { value: new Vector3(canvas.width, canvas.height, 1) },
+};
+
+const camera = new PerspectiveCamera();
+const scene = new SeedScene(camera);
+
 // Set up controls
 /*const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
@@ -31,10 +44,37 @@ controls.enablePan = false;
 //controls.maxDistance = 16;
 controls.update();*/
 
+// Bloom Effect
+/*
+const params = {
+    exposure: 1,
+    bloomStrength: 0.2,
+    bloomThreshold: 0.8,
+    bloomRadius: 0
+};
+
+
+const renderPass = new RenderPass(scene, camera);
+
+const bloomPass = new UnrealBloomPass( new Vector2( innerWidth, innerHeight ), 1.5, 0, 0 );
+bloomPass.threshold = params.bloomThreshold;
+bloomPass.strength = params.bloomStrength;
+bloomPass.radius = params.bloomRadius;
+
+const composer = new EffectComposer( renderer );
+composer.addPass( renderPass );
+composer.addPass( bloomPass );
+*/
+
 // Render loop
 const onAnimationFrameHandler = (timeStamp) => {
+
+    global.uniforms.iTime.value = timeStamp * 0.001;
+
+
     //controls.update();
     renderer.render(scene, camera);
+    //composer.render();
     scene.update && scene.update(timeStamp);
     window.requestAnimationFrame(onAnimationFrameHandler);
 };
--- src/components/objects/Road/Road.js
+++ src/components/objects/Road/Road.js
@@ -1,4 +1,4 @@
-import { Group, RingBufferGeometry, MeshBasicMaterial, Mesh, Scene } from 'three';
+import { Group, RingBufferGeometry, MeshBasicMaterial, Mesh, Scene, ShaderMaterial } from 'three';
 
 class Road extends Group {
     constructor(parent) {
@@ -8,7 +8,7 @@ class Road extends Group {
         this.name = 'road';
 
         // Geometry of the track
-        const roadGeometry = new RingBufferGeometry(30, 70, 40, 40, 0, 6.3);
+        const roadGeometry = new RingBufferGeometry(30, 70, 40, 20, 0, 6.283185);
         roadGeometry.rotateX(3 * (Math.PI / 2));
         this.geometry = roadGeometry;
 
@@ -18,7 +18,45 @@ class Road extends Group {
         this.material = roadMaterial;
 
         // Add the track to the scene
-        const track = new Mesh(roadGeometry, roadMaterial);
+        //const track = new Mesh(roadGeometry, roadMaterial);
+
+
+        // ShaderToy - uses code from https://threejsfundamentals.org/threejs/lessons/threejs-shadertoy.html
+
+        // Fragment shader - Rainbow
+        const fragmentShader = `
+        #include <common>
+        
+        uniform vec3 iResolution;
+        uniform float iTime;
+        
+        // By iq: https://www.shadertoy.com/user/iq  
+        // license: Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License.
+        void mainImage( out vec4 fragColor, in vec2 fragCoord )
+        {
+            // Normalized pixel coordinates (from 0 to 1)
+            vec2 uv = fragCoord/iResolution.xy;
+        
+            // Time varying pixel color
+            vec3 col = 0.5 + 0.5*cos(iTime+uv.xyx+vec3(0,2,4));
+        
+            // Output to screen
+            fragColor = vec4(col,1.0);
+        }
+        
+        void main() {
+        mainImage(gl_FragColor, gl_FragCoord.xy);
+        }
+        `;
+        let uniforms = global.uniforms;
+
+        const material = new ShaderMaterial({
+            fragmentShader,
+            uniforms,
+        });
+
+        const track = new Mesh(roadGeometry, material);
+
         parent.add(track);
     }
 }
